8e20591774466cf4b6d611dec799d45f2c113aa7
Jay-son Alburg <Jay-son@jay-sons-macbook-pro.local>
2019-01-19 17:44:06 -0500

    Initial Commit

---
+++ TippedTests/TippedTests.swift
@@ -0,0 +1,34 @@
+//
+//  TippedTests.swift
+//  TippedTests
+//
+//  Created by Jay-son Alburg on 1/19/19.
+//  Copyright © 2019 Jay'son Alburg. All rights reserved.
+//
+
+import XCTest
+@testable import Tipped
+
+class TippedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
